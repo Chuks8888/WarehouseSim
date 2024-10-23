8291a6313922b3e2a4e742b09872a771e3d096d6
Chuks8888 <hugotarczynski@gmail.com>
2024-10-23 23:33:07 +0200

    initial commit

---
+++ Source/Warehouse/GridSetup.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GridSetup.h"
+#include "Engine/Blueprint.h"
+#include "Engine/World.h"
+#include "Editor/EditorEngine.h"
+
+// Sets default values
+AGridSetup::AGridSetup()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AGridSetup::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AGridSetup::SpawnObjects(UBlueprint* Actor, FString World, int width, int length, bool& OutputSuccess)
+{
+	if (!Actor)
+	{
+		OutputSuccess = false;
+		return;	
+	}
+
+	if (!Actor->GeneratedClass->IsChildOf(AActor::StaticClass()))
+	{
+		OutputSuccess = false;
+		return;
+	}
+
+	UWorld* world = nullptr;
+	world = Cast<UWorld>(StaticLoadObject(UWorld::StaticClass(), nullptr, *World));
+
+	if (!world)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::FString("world not loaded"));
+		OutputSuccess = false;
+		return;
+	}
+
+	FVector spawnloc = FVector();
+	for (int i = 0; i < length; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			spawnloc.Set(0.0f + 100.0f * j, 0.0f + 100.0f * i, 0.0f);
+			world->SpawnActor(Actor->GeneratedClass, &spawnloc);
+		}
+	}
+}
+
+void AGridSetup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/Warehouse/GridSetup.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GridSetup.generated.h"
+
+UCLASS(Blueprintable)
+class WAREHOUSE_API AGridSetup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	
+	AGridSetup();
+
+protected:
+	
+	virtual void BeginPlay() override;
+
+public:	
+	UFUNCTION(BlueprintCallable)
+		void SpawnObjects(UBlueprint* Actor, FString World, int width, int length, bool& OutputSuccess);
+
+	virtual void Tick(float DeltaTime) override;
+
+};
